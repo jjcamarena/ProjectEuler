e54e7ae821197f2cef823b6c88de0fdfb16b9a56
Pepe Camarena <elpatiodepepe@gmail.com>
2024-10-03 13:21:54 -0600

    MultiplesOf3or5 done.

--- ProjectEuler/MultiplesOf3or5.java
+++ ProjectEuler/MultiplesOf3or5.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MultiplesOf3or5 {
     public static void main(String[] args) {
+        ArrayList<Integer> multiples = new ArrayList<>();
 
+        for (int i = 999; i>= 1; i--) {
+            if (i%3 == 0 || i%5 == 0)
+                multiples.add(i);
+        }
+
+        Integer sum = multiples.stream()
+                .reduce(0, Integer::sum);
+
+        System.out.println("La suma es: " + sum);
     }
 }
